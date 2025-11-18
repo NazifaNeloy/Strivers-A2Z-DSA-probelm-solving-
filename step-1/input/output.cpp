fbb2d37e476eda252292524d992cb552cd7ac1f2
Nazifa Neloy <nazifaneloy19@gmail.com>
2025-11-18 11:12:18 +0600

    Create output.cpp

---
+++ step-1/input/output.cpp
@@ -0,0 +1,25 @@
+#include<bits./stdio.h>
+
+using namespace std;
+
+int main {
+    int x;
+    cin>>x;
+    cout<<x<<endl;
+    return 0;
+}
+
+
+
+
+
+
+// class Solution {
+// public:
+//     void printNumber() {
+//         int x;
+//         cin>>x;
+//         cout<<x<<endl;
+
+//     }
+// };
